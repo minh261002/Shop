aaaf52b6d29c72a79dfc6147820e4e821c564a66
minhttran <trcongminh.261002@gmail.com>
2024-09-14 08:00:37 +0700

    Update cors config & database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\UserCatalogue;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,7 +16,13 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        UserCatalogue::create([
+            'name' => 'Administator',
+            'description' => 'Quản trị viên hệ thống',
+        ]);
+
         User::factory()->create([
+            'user_catalogue_id' => 1,
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('password'),
